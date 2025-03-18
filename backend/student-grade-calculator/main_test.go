3ecb91cc37083dda943e0c311ce4cc08eed2abc7
JABEZ8650 <eyumuhaba@gmail.com>
2025-03-18 11:35:31 +0300

    initial commit

---
+++ backend/student-grade-calculator/main_test.go
@@ -0,0 +1,18 @@
+// package main
+
+// import "testing"
+
+// func TestCalculateAverage(t *testing.T) {
+// 	grades := map[string]float64{
+// 		"Math":    90,
+// 		"Science": 80,
+// 		"History": 85,
+// 	}
+
+// 	got := calculateAverage(grades)
+// 	want := 85.0
+
+// 	if got != want {
+// 		t.Errorf("Expected average %v, but got %v", want, got)
+// 	}
+// }
