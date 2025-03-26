2130c3ca777f5630d3259b23b37a9709a22f05e5
JABEZ8650 <eyumuhaba@gmail.com>
2025-03-26 05:35:22 +0300

    student-grade-calculator finalized

--- backend/student-grade-calculator/main.go
+++ backend/student-grade-calculator/main.go
@@ -1,1 +1,65 @@
-// make it look like the image
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func average(grades map[string]float64) float64 {
+	var sum float64
+	var count int
+
+	for _, mark := range grades {
+		sum += mark
+		count++
+	}
+	if count == 0 {
+		return 0
+	}
+
+	return sum / float64(count)
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Welcome to Student grade calculator!")
+
+	fmt.Print("Enter your name: ")
+	studentName, _ := reader.ReadString('\n')
+	studentName = studentName[:len(studentName)-1]
+
+	fmt.Print("How many classes do you take: ")
+	var subjectNumber int
+	fmt.Scanln(&subjectNumber)
+
+	marklist := make(map[string]float64)
+	for i := 1; i <= subjectNumber; i++ {
+		var subjectName string
+		var grade float64
+
+		fmt.Printf("Enter subject #%d name: ", i)
+		fmt.Scanln(&subjectName)
+
+		for {
+			fmt.Printf("Enter %s grade(0-100): ", subjectName)
+			fmt.Scanln(&grade)
+
+			if grade <= 100 && grade >= 0 {
+				break
+			} else {
+				println("Enter a valid number between 1 and 100")
+			}
+		}
+		marklist[subjectName] = grade
+	}
+
+	fmt.Printf("\nGrade Summery\n")
+	for subj, mark := range marklist {
+		fmt.Printf("%s: %.2f\n", subj, mark)
+	}
+
+	ave_grade := average(marklist)
+	fmt.Printf("%s's average grade is %.2f\n", studentName, ave_grade)
+}
--- backend/student-grade-calculator/main_test.go
+++ backend/student-grade-calculator/main_test.go
@@ -1,18 +1,18 @@
-// package main
+package main
 
-// import "testing"
+import "testing"
 
-// func TestCalculateAverage(t *testing.T) {
-// 	grades := map[string]float64{
-// 		"Math":    90,
-// 		"Science": 80,
-// 		"History": 85,
-// 	}
+func averageCalculaterTest(t *testing.T) {
+	grades := map[string]float64{
+		"math": 75,
+		"eng":  65,
+		"phy":  70,
+	}
 
-// 	got := calculateAverage(grades)
-// 	want := 85.0
+	got := average(grades)
+	want := 70.0
 
-// 	if got != want {
-// 		t.Errorf("Expected average %v, but got %v", want, got)
-// 	}
-// }
+	if got != want {
+		t.Errorf("Expected value is %v, but got %v", want, got)
+	}
+}
