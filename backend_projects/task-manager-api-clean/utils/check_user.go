762f7726cdf66f92be83f5993573cd10191c350a
jabez8650 <eyumuhaba@gmail.com>
2025-08-18 09:57:49 +0300

    domain and utils

---
+++ backend_projects/task-manager-api-clean/utils/check_user.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"errors"
+	"task-manager-api-clean/domain"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUser(c *gin.Context) (*domain.AuthenticatedUser, error) {
+	value, exist := c.Get("AuthenticatedUser")
+	if !exist {
+		return nil, errors.New("user not found")
+	}
+	currUser, ok := value.(*domain.AuthenticatedUser)
+	if !ok {
+		return nil, errors.New("user not found in context")
+	}
+	return currUser, nil
+}
